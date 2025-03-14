3a2f22e001f4318f27b4513b6dbd38927ba53758
Orolling <Orolling@163.com>
2025-03-15 00:31:41 +0800

    init project

---
+++ OneSentence/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for OneSentence project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OneSentence.settings')
+
+application = get_wsgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # One-sentence
 One sentence for yourself, your lover, your family, your friends, etc.
+
+
+# env
+```python3.11```
+```django4.2```
